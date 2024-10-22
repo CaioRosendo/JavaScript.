73f7c7a3f4d067f38337db9e86b1aa1a61b29e33
Caio Henrique Rosendo da Fonseca <ocaiohenrique@gmail.com>
2024-10-22 16:18:29 -0300

    condicoes

--- exercícios/001/ex001/script.js
+++ exercícios/001/ex001/script.js
@@ -1,17 +1,17 @@
-function Carregar(){
-    var msg = window.document.getElementById('msg')
-    var img = window.document.getElementById('imagem')
+function Carregar() {
+    var msg = document.querySelector('div#msg')
+    var img = document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
     msg.innerHTML = `Agora são ${hora} horas`
-    if(hora >= 0 && hora < 12) {
-        img.src = 'imagens/manha.png'
+    if (hora >= 0 && hora < 12) {
+        img.src ='imagens/manha.png'
         document.body.style.backgroundColor = '#e2cd9f'
-    } else if(hora >=12 && hora < 18) {
+    } else if (hora >= 12 && hora < 18) {
         img.src = 'imagens/tarde.png'
         document.body.style.backgroundColor = '#b9846f'
     } else {
         img.src = 'imagens/noite.png'
         document.body.style.backgroundColor = '#515154'
-}
+    }
 }
