936062e3cc54d22dd90e47c8e80bd4a74a33a756
Caio Henrique Rosendo da Fonseca <109710838+CaioRosendo@users.noreply.github.com>
2024-11-25 18:33:19 -0300

    .

--- aula11/ex008.js
+++ aula11/ex008.js
@@ -1,6 +1,6 @@
-var vel = 60
+var vel = 61
 console.log(`A velocidade é ${vel} Km/h`)
 if(vel > 60) {
     console.log(`Você ultrapassou em ${vel - 60} a velocidade permitida, por isso será multado.`)
-}
+} 
 console.log(`Dirija sempre usando cinto de seguraça.`)
